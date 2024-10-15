fc2e8d0b97670eb5eaee065db9d884268694a6f4
Thomas <thomas.filhol@edu.devinci.fr>
2024-10-15 13:59:20 +0200

    Initial Commit

---
+++ SwiftUICours/SwiftUICoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoursApp.swift
+//  SwiftUICours
+//
+//  Created by FILHOL Thomas on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
